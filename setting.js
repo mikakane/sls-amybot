2223eb5977bb06bd74439983d7ed9166837e9eb2
mikakane <mikakane2@gmail.com>
2016-11-18 12:27:29 +0900

    update

--- setting.js
+++ setting.js
@@ -1,8 +1,10 @@
 "use strict"
 
 const loadenv = require("node-env-file");
-loadenv("./.env")
-
+try{
+    loadenv("./.env")
+}catch (e){
+}
 module.exports = {
     SLACK_TOKEN: process.env.SLACK_TOKEN,
     SLACK_ICONEMOJI: ":aim:",
