c3114ae8b1eaf0fdb1a16bdd364bc794be48c66e
t.goto <t.goto@chatbox-inc.com>
2016-12-27 18:48:58 +0900

    修正

--- handler.js
+++ handler.js
@@ -6,20 +6,3 @@ module.exports.backlog = require("./src/actions/backlog.js");
 
 module.exports.jiho = require("./src/actions/jiho.js");
 
-// module.exports.line = (event, context, callback) => {
-//   request
-//       .post('https://notify-api.line.me/api/notify')
-//       .send({ message: 'ねえねぇ' })
-//       .set('Content-Type', 'application/x-www-form-urlencoded')
-//       .set('Authorization', 'Bearer '+TOKEN)
-//       .end(function(err, res){
-//         const response = {
-//           statusCode: 200,
-//           body: JSON.stringify({
-//             message: 'Go Serverless v1.0! Your function executed successfully!',
-//           })
-//         };
-//         callback(null, response);
-//       })
-// };
-
